0dd837c2d7e9d4cf4f582b2799c1f482c901ff54
Aizat Ibraimova <97326728+aizatibraimova@users.noreply.github.com>
2024-03-04 20:04:42 +0000

    rps game

--- app.rb
+++ app.rb
@@ -1,8 +1,52 @@
-require 'sinatra'
+require "sinatra"
+require "sinatra/reloader"
 
-get('/') do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/") do
+ erb(:homepage)
+end
+
+get("/rock") do 
+moves = ["rock", "paper", "scissors"]
+@comp_move = moves.sample
+
+if @comp_move == "rock"
+  @outcome = "tied"
+elsif @comp_move == "paper"
+  @outcome = "lost"
+else
+  @outcome = "won"
+end
+
+erb(:zebra)
+end
+
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+@comp_move = moves.sample
+
+if @comp_move == "paper"
+  @outcome = "tied"
+elsif @comp_move == "scissors"
+  @outcome = "lost"
+else
+  @outcome = "won"
+end
+
+erb(:giraffe)
+end
+
+get("/scissors") do 
+  moves = ["rock", "paper", "scissors"]
+@comp_move = moves.sample
+
+if @comp_move == "scissors"
+  @outcome = "tied"
+elsif @comp_move == "rock"
+  @outcome = "lost"
+else
+  @outcome = "won"
+end
+
+erb(:lion)
 end
